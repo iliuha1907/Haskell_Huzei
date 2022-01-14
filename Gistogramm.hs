d7f90a63df41539bf3fa679232c79448de5bea9b
iliuha1907 <60312075+iliuha1907@users.noreply.github.com>
2022-01-14 14:32:41 +0300

    Delete Gistogramm.hs

--- Gistogramm.hs
+++
@@ -1,7 +0,0 @@
-import qualified Data.ByteString as B
-main = do
-  s <- B.readFile "Example.dat"
-  calculateBytes s
-
-calculateBytes :: B.ByteString -> IO ()
-calculateBytes theString = print $ Prelude.map (\x -> (B.head x, B.length x)) $ B.group theString
