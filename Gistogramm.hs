07d7da83ad2d5473f29545f5ea068bc8529dccb0
iliuha1907 <60312075+iliuha1907@users.noreply.github.com>
2022-01-14 14:32:54 +0300

    Add files via upload

---
+++ Gistogramm.hs
@@ -0,0 +1,7 @@
+import qualified Data.ByteString as B
+main = do
+  s <- B.readFile "Example.dat"
+  calculateBytes s
+
+calculateBytes :: B.ByteString -> IO ()
+calculateBytes theString = print $ Prelude.map (\x -> (B.head x, B.length x)) $ B.group $ B.sort theString
